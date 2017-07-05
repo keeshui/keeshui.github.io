cd6acde52bc2179968553b3a872a0e3789f02239
Kees Hui <keeshui@zeelandnet.nl>
2017-07-05 20:22:49 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,20 @@ myImage.onclick = function() {
     }
 }
 var myButton = document.querySelector('button');
+var myButt2 = document.querySelector('button' name='butt2');
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
   myHeading.textContent = myName  
 }
+
+function setUserNam2() {
+  var myNam2 = prompt('Please naam2.');
+  localStorage.setItem('name', myNam2);
+  myHeading.textContent = myNam2  
+}
+// kill all about nam2's
 
 if(!localStorage.getItem('name')) {
   setUserName();
@@ -30,6 +38,10 @@ myButton.onclick = function() {
   setUserName();
 }
 
+// to kill nam2 
+myBut2.onclick = function() {
+  setUserNam2();
+}
 
 
 
