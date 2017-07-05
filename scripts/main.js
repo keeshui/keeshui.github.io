a3910eddab8f1572c14a391f7b817fd5663f99b4
Kees Hui <keeshui@zeelandnet.nl>
2017-07-05 18:24:25 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/publi.png);
+      myImage.setAttribute ('src','images/publi.png');
     } else {
       myImage.setAttribute ('src','images/firefox-icon.png');
     }
