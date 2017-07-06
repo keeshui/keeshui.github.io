a264fdf26c5e9bb29fb5250bf7515155252bb60b
Kees Hui <keeshui@zeelandnet.nl>
2017-07-06 20:56:19 +0200

    Add files via upload

--- scripts/Kopie van main.js
+++ scripts/Kopie van main.js
@@ -1,54 +1,37 @@
-/* go see firfo pag src
-// for site (public=free,else 7/mnd)at ..?.. keeshui.github.io :
-// keeshui.github.io
-// gos https://keeshui.github.io/ and shows only: 
-// keeshui.github.io		in Fat Big	  ( + bijlagig=paperclip on Touch )  	
-// just a start try own page	in normal char
-// =========================	-------------
-4-7-17: spelfout in storedName correct met myButton cattenates new tot reload, then just new. err-1!
-err-1 must be in fun setUserName();
-===================== */
 var myHeading = document.querySelector('h1');
- // myHeading.textContent = 'Mozilla is cool, ' + myName;
-myHeading.textContent = 'Hello world!, ';
+myHeading.textContent = 'Hello world kh!, ';
 
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-//   myImage.setAttribute ('src','images/publi.png'); //e-5? taken at home BUT: at disk is .PNG, @Site matters?
-//     myImage.setAttribute ('src','images/publi.PNG); //oeink e-5, broken works at home; also i n index.html?!!
-      myImage.setAttribute ('src','images/publi.PNG);
-//    publi.PNG nb was firefox2.png'); 
+//    if(mySrc === 'images/firefox-icon.png') {
+   if(mySrc === 'images/varia2.JPG') {
+      myImage.setAttribute ('src','images/publi.png');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+//      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','images/varia2.JPG');
     }
 }
 var myButton = document.querySelector('button');
-// var myHeading = document.querySelector('h1');
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-// myHeading.textContent = 'Mozilla is cool, ' + myName;
-// err1  myHeading.textContent = myHeading.textContent + myName  // + "br"; 
-// err-2?:myBut,,cancel -> nul-lifies storedName
-  myHeading.textContent = myName  // don e-1
+  myHeading.textContent = myName  
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-// myHeading.textContent = 'Mozilla is cool, ' + storedName;
   myHeading.textContent = myHeading.textContent  + storedName;
 }
 
 myButton.onclick = function() {
   setUserName();
 }
-// err-3 type iets of niets, cancel kills Heading en
-// err-4 type iets, ok kills pre-part = just nam
+
+
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,27 @@
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world kh!, ';
+// myHeading.textContent = 'Hello world !, kh ';
 
 var myImage = document.querySelector('img');
+var arr = ['publi.png', 'varia2.JPG', 'Ras11x9-varia.JPG','pi7-jx9-af.JPG', "pi5-jx9-af.JPG",'compa256p2txt.png', 'Kul-2-11x9.JPG', 'firefox-icon.png' ];
+// was lastig firefox-icon.png' ]; met parts transparant oid, KH, no ? was by blank 1st char ' fir..'
+
+var aL = arr.length;
+var i = 1;
+var ccP = 0; // counts clicks on Pictures
 
 myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-//    if(mySrc === 'images/firefox-icon.png') {
-   if(mySrc === 'images/varia2.JPG') {
-      myImage.setAttribute ('src','images/publi.png');
-    } else {
-//      myImage.setAttribute ('src','images/firefox-icon.png');
-      myImage.setAttribute ('src','images/varia2.JPG');
-    }
+   var mySrc = myImage.getAttribute('src');
+   cl(  mySrc + ' repl. by: ' + arr[i] );  ///?on\ipv/ en nog on this line //it solves?
+   myImage.setAttribute ('src',  'images/' + arr[i] );
+   i=i+1; ccP++;
+   cl( 'i= ' + i );
+   if ( i == aL ){ i=0;   // === cannot
+   }
+   cl( 'i=' + i );
+// cl(ccP); //temp tes ++, ok
+ if ( ccP === 1 ){ myHeading.textContent = 'Hello world !, kh ' + aL + ' Paints' ; }
 }
+
 var myButton = document.querySelector('button');
 
 function setUserName() {
@@ -20,6 +29,9 @@ function setUserName() {
   localStorage.setItem('name', myName);
   myHeading.textContent = myName  
 }
+function cl(a ){ console.log(a); }
+// =============
+cl(aL + ' prents, kh');  //nb
 
 if(!localStorage.getItem('name')) {
   setUserName();
